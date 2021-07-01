3215dcf8433124f15e49c05bc70c67c3736aa52d
xmmmmmovo <13256172360@163.com>
2021-07-01 09:04:13 +0800

    first commit

---
+++ src/index.d.ts
@@ -0,0 +1,18 @@
+// Memory extension samples
+declare interface Memory {
+  uuid: number;
+  log: any;
+}
+
+declare interface CreepMemory {
+  role: string;
+  room: string;
+  working: boolean;
+}
+
+// Syntax for adding proprties to `global` (ex "global.log")
+declare namespace NodeJS {
+  interface Global {
+    log: any;
+  }
+}
