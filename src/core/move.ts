96cd704a1aca994fef4537f5b9b07596af8c060d
xmmmmmovo <13256172360@163.com>
2022-04-26 03:35:23 +0800

    对穿2

--- src/core/move.ts
+++ src/core/move.ts
@@ -4,7 +4,50 @@ interface Creep {
 
 Creep.prototype.customMove = function (target: RoomPosition, range = 1, ignoreCreep = true): ScreepsReturnCode {
   if (!this.memory.path || !this.memory.path.length) {
-    return OK
+    const res = PathFinder.search(
+      this.pos,
+      {
+        pos: target,
+        range
+      },
+      {
+        plainCost: 2,
+        swampCost: 10,
+        roomCallback(roomName): CostMatrix | boolean {
+          const room = Game.rooms[roomName]
+          // 在这个示例中，`room` 始终存在
+          // 但是由于 PathFinder 支持跨多房间检索
+          // 所以你要更加小心！
+          if (!room) return false
+          const costs = new PathFinder.CostMatrix()
+          room.find(FIND_STRUCTURES).forEach(function (struct) {
+            if (struct.structureType === STRUCTURE_ROAD) {
+              // 相对于平原，寻路时将更倾向于道路
+              costs.set(struct.pos.x, struct.pos.y, 1)
+            } else if (
+              struct.structureType !== STRUCTURE_CONTAINER &&
+              (struct.structureType !== STRUCTURE_RAMPART || !struct.my)
+            ) {
+              // 不能穿过无法行走的建筑
+              costs.set(struct.pos.x, struct.pos.y, 0xff)
+            }
+          })
+          room.find(FIND_MY_CONSTRUCTION_SITES).forEach(function (site) {
+            costs.set(site.pos.x, site.pos.y, 0xff)
+          })
+          // 躲避房间中的 creep
+          if (!ignoreCreep) {
+            room.find(FIND_CREEPS).forEach(function (creep) {
+              costs.set(creep.pos.x, creep.pos.y, 0xff)
+            })
+          }
+          return costs
+        },
+        maxOps: 5000,
+        maxRooms: 8
+      }
+    )
+    this.memory.path = res.path
   } else if (this.pos.x === this.memory.path[0].x && this.pos.y === this.memory.path[0].y) {
     this.memory.path.shift()
   }
